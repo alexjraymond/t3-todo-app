4c1e0703e3cdad0199d04be93c58f1e9e8394bd6
Alex Raymond <alexjacobraymond@gmail.com>
2023-04-03 16:39:14 -0700

    more navbar stuff

--- src/pages/NavBar.tsx
+++ src/pages/NavBar.tsx
@@ -1,4 +1,10 @@
-import { AddIcon, SearchIcon, HamburgerIcon } from "@chakra-ui/icons";
+import {
+  AddIcon,
+  SearchIcon,
+  HamburgerIcon,
+  Icon,
+  CloseIcon,
+} from "@chakra-ui/icons";
 import {
   Box,
   Flex,
@@ -9,50 +15,107 @@ import {
   Avatar,
   MenuButton,
   Menu,
+  Text,
+  useDisclosure,
 } from "@chakra-ui/react";
 import { faSeedling } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import React from "react";
+import React, { ReactNode, useState } from "react";
 
 const NavBar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const toggle = () => setIsOpen(!isOpen);
+
+  return <NavContainer />;
+};
+
+const NavAvatar = () => {
   return (
-    <Box as="nav" bg="tomato" className="fixed w-full" px={4}>
+    <Avatar
+      name="Dan Abrahmov"
+      src="https://bit.ly/dan-abramov"
+      size="sm"
+      className="ring-1 ring-white"
+    />
+  );
+};
+
+const NavSearch = () => {
+  return (
+    <IconButton
+      variant="ghost"
+      colorScheme="whiteAlpha"
+      aria-label="new task"
+      icon={<SearchIcon />}
+      size="sm"
+      _hover={{
+        background: "white",
+        color: "tomato",
+      }}
+    />
+  );
+};
+
+const NavNewTask = () => {
+  return (
+    <IconButton
+      variant="ghost"
+      colorScheme="whiteAlpha"
+      aria-label="new task"
+      icon={<AddIcon />}
+      size="sm"
+      _hover={{
+        background: "white",
+        color: "tomato",
+      }}
+    />
+  );
+};
+
+const Logo = () => {
+  return <Text>NotTodoist</Text>;
+};
+
+const MenuItems = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  return (
+    <Menu>
+      <MenuButton
+        as={IconButton}
+        icon={isOpen ? <CloseIcon /> : <HamburgerIcon />}
+        variant="ghost"
+        colorScheme="whiteAlpha"
+        _hover={{
+          background: "white",
+          color: "tomato",
+        }}
+        size={"sm"}
+        fontSize={"20px"}
+        aria-label={"Open Menu"}
+        display={{ md: "none" }}
+        onClick={isOpen ? onClose : onOpen}
+      />
+      <NavNewTask />
+      <NavSearch />
+      <NavAvatar />
+    </Menu>
+  );
+};
+
+const NavContainer = ({ ...props }) => {
+  return (
+    <Box
+      as="nav"
+      bg="tomato"
+      className="fixed left-0 top-0 m-0 w-full shadow-md"
+      px={4}
+    >
       <Flex minH="40px" justifyContent="space-between">
         <Center color="white" fontWeight="bold">
-          <text>NotTodoist</text>
+          <Logo />
         </Center>
-        <HStack>
-          <Center justifyContent="space-between" gap={2}>
-            <Menu>
-              {/* <MenuButton
-                as={IconButton}
-                aria-label="Options"
-                icon={<HamburgerIcon />}
-                variant="ghost"
-              /> */}
-
-              <IconButton
-                variant="ghost"
-                colorScheme="whiteAlpha"
-                aria-label="new task"
-                icon={<SearchIcon />}
-                size="sm"
-              />
-              <IconButton
-                variant="ghost"
-                colorScheme="whiteAlpha"
-                aria-label="new task"
-                icon={<AddIcon />}
-                size="sm"
-              />
-              <Avatar
-                name="Dan Abrahmov"
-                src="https://bit.ly/dan-abramov"
-                size="sm"
-                className="ring-1 ring-white"
-              />
-            </Menu>
-          </Center>
+        <HStack spacing={"24px"}>
+          <MenuItems />
         </HStack>
       </Flex>
     </Box>
